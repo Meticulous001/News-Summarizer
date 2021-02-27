81d86de2596659e565d4b778918bdf75e5d4c804
Raheem Muiz Olalekan <20601466+Meticulous001@users.noreply.github.com>
2021-02-27 15:34:13 +0100

    Add files via upload

---
+++ Webpage_text_summarization.R
@@ -0,0 +1,32 @@
+setwd("C:\\Users\\JANE ODUM\\Desktop\\Lab\\Text-mining")
+
+library(xml2)
+library(rvest)
+library(lexRankr)
+
+set.seed(103)
+
+###give the url
+file_url <- readline(prompt = "Enter the URL: ")
+
+#file_url = "https://punchng.com/contempt-court-sends-ncac-boss-runsewe-to-prison/"
+
+page = xml2::read_html(file_url)
+
+page_text = rvest::html_text(rvest::html_nodes(page, "p"))
+
+
+###using top rank for top 4 summary
+top_4 <- lexRankr::lexRank(page_text,
+                           docId = rep(1, length(page_text)), n = 4, continuous = T)
+
+
+order_of_appearance <- order(as.integer(gsub("_","", top_4$sentenceId)))
+
+
+###show top 4 headlines
+
+order_top_4 <- top_4[order_of_appearance, "sentence"]
+
+
+print(top_4$sentence)
